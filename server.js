88ff4f8c9623b95d01d8f24077aa47aab360d5a0
daddyg <mike.godber@gmail.com>
2012-01-27 17:48:43 +0000

    Starting to use a db

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var http = require('http');
 var express = require('express');
 var blackjack = require('./blackjack');
+var mongo = require('mongodb');
 
+var mongoBlackjack = new mongo.Db('blackjack', new mongo.Server("127.0.0.1", 27017, {}),
+    test = function (err, collection) {
+      collection.insert({a:2}, function(err, docs) {
+
+        collection.count(function(err, count) {
+          
+        });
+
+        // Locate all the entries using find
+        collection.find().toArray(function(err, results) {
+          
+
+          // Let's close the db
+          mongoBlackjack.close();
+        });
+      });
+    });
+
+mongoBlackjack.open(function(err, p_client) {
+  mongoBlackjack.collection('test_insert', test);
+});
 
 var hosting = {
 	port : process.argv[2] 
@@ -20,20 +42,24 @@ application.configure(function() {
 
 });
 
-var hand;
 application.get('/', function(req, res){
-	hand = new blackjack.Hand();
+	var hand = new blackjack.Hand();
+	
+	
 	
 	res.render('index.jade', hand);
 });
 
 application.get('/twist', function(req, res){
+	var hand = getHand(req.data.id);
 	hand.draw();
 	
 	res.render('index.jade', hand);
 });
 
 application.get('/stick', function(req, res){
+	var hand = getHand(req.data.id);
+	
 	var dealer = new blackjack.Hand();
 	
 	while(dealer.score() <= 16)
@@ -41,5 +67,9 @@ application.get('/stick', function(req, res){
 		
 	res.render('result.jade', {you:hand, dealer:dealer});
 });
+
+function getHand(id){
+	return null;
+}
 
 application.listen(hosting.port);
